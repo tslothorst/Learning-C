4a8a3b0f3882286df87223f36d1cfdaba7156208
tslothorst <tslothorst@gmail.com>
2020-01-06 09:18:42 +0100

    Added concatenate function

--- Section10/Challenge1/main.c
+++ Section10/Challenge1/main.c
@@ -9,12 +9,17 @@ int main() {
     char string1[] = "hiya!";
     char string2[] = "hello";
     char string3[] = "world!";
+    char string4[length(string2) + length(string3) + 1];
+
+    concatenate(string4,string2,string3);
 
     printf("The length of hello is: %d\n",length(string2)); // Should be 5.
     printf("The length of hello is: %d\n",length(string3)); // Should be 6.
     printf("The string is the same: %d\n", compare(string2,string2)); // Should print 1 for true
     printf("The string is the same: %d\n", compare(string2,string3)); // Should print 0 for false
     printf("The string is the same: %d\n", compare(string2,string1)); // Should print 0 for false
+    printf("%s", string4); // Should print "helloworld!"
+
 
     return 0;
 }
@@ -29,7 +34,15 @@ int length(char str[]){
 
 void concatenate(char result[], char str1[], char str2[]){
     int size = length(str1) + length(str2) + 1;
-
+    int i = 0;
+    for (i ; i < length(str1); ++i) {
+        result[i] = str1[i];
+    }
+    for (int k = 0; k < length(str2) ; ++k) {
+        result[i] = str2[k];
+        ++i;
+    }
+    result[size] = '\0';
 }
 
 bool compare(char str1[], char str2[]){
