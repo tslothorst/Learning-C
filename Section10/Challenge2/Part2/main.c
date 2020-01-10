875ddf2c1c87e2690f486aa1e6a0c0dc902bf50c
tslothorst <tslothorst@gmail.com>
2020-01-10 11:39:06 +0100

    Finished pt2

--- Section10/Challenge2/Part2/main.c
+++ Section10/Challenge2/Part2/main.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+void sortStrings(char arr[][100], int n);
 
 int main() {
+    char stringarr[][100] = { "zero","one","two" };
+
+    int n = sizeof(stringarr)/sizeof(stringarr[0]);
+    sortStrings(stringarr, n);
+
+    printf("The strings appears after sorting: ");
+    for (int i=0; i<n; i++){
+        printf("\n%s", stringarr[i]);
+    }
+
     return 0;
+}
+
+void sortStrings(char arr[][100], int n)
+{
+    char temp[100];
+
+    // Sorting strings using bubble sort
+    for (int j=0; j<n-1; j++)
+    {
+        for (int i=j+1; i<n; i++)
+        {
+            if (strcmp(arr[j], arr[i]) > 0)
+            {
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[i]);
+                strcpy(arr[i], temp);
+            }
+        }
+    }
 }
