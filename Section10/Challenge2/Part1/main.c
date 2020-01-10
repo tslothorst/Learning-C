f92e9a6edc2aa623b9ce968ffaa99b3ea53b3f33
tslothorst <tslothorst@gmail.com>
2020-01-10 10:17:24 +0100

    Completed pt1

--- Section10/Challenge2/Part1/main.c
+++ Section10/Challenge2/Part1/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    char temp[100];
+    printf("Type in a string: ");
+    scanf("%s",temp);
+    _strrev(temp);
+    printf("\nString in reverse:\n");
+    printf("%s", temp);
     return 0;
 }
