3b57fcc5ebc75fd8dd5a6f8a7a5b52b378bc4388
tslothorst <tslothorst@gmail.com>
2020-01-11 16:22:34 +0100

    Completed Section 12 challenge 1

--- Section12/Challenge1/main.c
+++ Section12/Challenge1/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main() {
+
+    int number = 27;
+    int* numptr = &number;
+
+    printf("The address of the pointer is: %p\n", &numptr);
+    printf("The value of the pointer is: %p\n", numptr);
+    printf("The value of what the pointer is pointing to: %d\n",*numptr);
+
     return 0;
 }
