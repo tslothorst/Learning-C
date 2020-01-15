b56c5dd40e904edd3cedda068dbe7dd08d501b15
tslothorst <tslothorst@gmail.com>
2020-01-15 09:53:11 +0100

    Removed unneeded extra pointer

--- Section12/Challenge4/main.c
+++ Section12/Challenge4/main.c
@@ -4,16 +4,13 @@
 
 int main() {
     char* input = (char*)calloc(80, sizeof(char));
-    char* output = NULL;
     if(input != NULL){
         printf("Enter something: ");
         gets(input);
-        output = (char*)realloc(input,strlen(input)+1);
-        printf("You entered: %s",output);
+        input = (char*)realloc(input,strlen(input)+1);
+        printf("You entered: %s",input);
     }
     free(input);
-    free(output);
     input = NULL;
-    output = NULL;
     return 0;
 }
