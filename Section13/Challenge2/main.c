8f120dfb0b10a318606f9b5c054488d5950a6635
tslothorst <tslothorst@gmail.com>
2020-01-19 16:41:07 +0100

    Added Section 13 Challenge 2

---
+++ Section13/Challenge2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+struct item{
+    char* itemName;
+    int quantity;
+    float price;
+    float amount;
+};
+
+void readItem(struct item* obj);
+void printItem(struct item* obj);
+
+int main() {
+
+    return 0;
+}
