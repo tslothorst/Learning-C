081fa9c92831b8f9b12b247d91df0a375e74a03f
tslothorst <tslothorst@gmail.com>
2020-01-25 17:54:16 +0100

    Added loop to read and convert chars to upper

--- Section14/Challenge2/main.c
+++ Section14/Challenge2/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
+    FILE* infile = fopen("..\\romeoandjuliet.txt","r");
+    if(infile == NULL){
+        printf("Failed to read input file.\n");
+        return(-1);
+    }
+    if(infile != NULL) {
+        FILE *outfile = fopen("..\\romeoandjuliet_to_upper.txt", "ab+");
+        while(!feof(infile)){
+            fputc(toupper(fgetc(infile)),outfile);
+        }
+    }
     return 0;
 }
