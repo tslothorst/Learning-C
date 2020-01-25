b14d85e79662f91a1f4914d02bb74ea5c4c00504
tslothorst <tslothorst@gmail.com>
2020-01-25 17:15:59 +0100

    Finished Challenge 1

--- Section14/Challenge1/main.c
+++ Section14/Challenge1/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main() {
+    FILE* infile = fopen("..\\romeoandjuliet.txt","r");
+    int linecount = 1;
+    if(infile == NULL){
+        printf("Error reading from file\n");
+        return(-1);
+    }
+    if(infile !=NULL){
+        char c;
+        while(!feof(infile)){
+           c = fgetc(infile);
+           if(c == '\n'){
+               ++linecount;
+           }
+        }
+        printf("This file contains %d lines\n",linecount);
+    }
     return 0;
 }
