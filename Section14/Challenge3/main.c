3946a9dafc26deac8972ff51d6ea485d24d0f0a8
tslothorst <tslothorst@gmail.com>
2020-01-26 15:36:45 +0100

    Added function to reverse file contents

--- Section14/Challenge3/main.c
+++ Section14/Challenge3/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    FILE* infile = fopen("..\\romeoandjuliet.txt","r");
+    if(infile == NULL){
+        printf("\nError opening file.\n");
+        return(-1);
+    }
+    if(infile != NULL) {
+        FILE* outfile = fopen("..\\romeoandjuliet_reverse.txt","ab+");
+        fseek(infile, 0, SEEK_END);
+        int len;
+        len = ftell(infile);
+        for (int i = len; i >= 0; --i) {
+            char c = fgetc(infile);
+            fputc(c,outfile);
+            fseek(infile,-i,SEEK_END);
+        }
+        fclose(infile);
+        fclose(outfile);
+    }
     return 0;
 }
